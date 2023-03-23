f7bc4a7a7e6e11eea0e7288d9f3197879478cd37
Vinoth R <vinothrwins@gmail.com>
2023-03-23 18:49:35 +0530

    first commit

---
+++ FirstProgram.java
@@ -0,0 +1,14 @@
+package com.nxtgen.java;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// sum of two numbers
+		int num1 = 10;
+		int num2 = 20;
+		System.out.println(num1 + num2);
+	}
+
+}
